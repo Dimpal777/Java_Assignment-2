48b9fed672569cf411863da53e91a28df15e2012
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-12 22:20:02 +0530

    Add files via upload

---
+++ EqualityOperators.java
@@ -0,0 +1,24 @@
+/**
+ * EqualityOperators
+ */
+public class EqualityOperators {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        
+        // Equal operator (==)
+        if (a == b) {
+            System.out.println("a is equal to b");
+        } else {
+            System.out.println("a is not equal to b");
+        }
+        
+        // Not equal operator (!=)
+        if (a != b) {
+            System.out.println("a is not equal to b");
+        } else {
+            System.out.println("a is equal to b");
+        }
+    }
+}
