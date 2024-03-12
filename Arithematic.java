85b4db1544968241f6a41b594effc17f51b2e794
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-12 16:35:06 +0530

    Add files via upload

---
+++ Arithematic.java
@@ -0,0 +1,14 @@
+
+// Arithematic Operators (+,-,/)
+
+class Arithematic {
+    public static int add(int num1,int num2){
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int operand1 = 12;
+        int operand2 = 10;
+       System.out.println("Result " + add(operand1,operand2)); 
+    }
+}
